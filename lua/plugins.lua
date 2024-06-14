2b3a6c588980880d28947b185cdc8049935f64ba
Yurii Bilous <seidzuro@gmail.com>
2024-06-14 19:28:50 +0300

    feat: add tmux-navigator

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -213,6 +213,24 @@ local plugins = {
         },
     },
 
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
+
     -- Whichkey
     {
         "folke/which-key.nvim",
