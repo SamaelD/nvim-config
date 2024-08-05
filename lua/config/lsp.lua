85d147c72ee3141339751d54bb294f5626fdc8ad
Yurii Bilous <seidzuro@gmail.com>
2024-08-06 01:44:30 +0300

    add keybindings to move lines

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -16,7 +16,7 @@ function _lsp.setup()
             map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
             map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
             map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-            map("K", vim.lsp.buf.hover, "[H]over [D]ocumentation")
+            map("hd", vim.lsp.buf.hover, "[H]over [D]ocumentation")
             map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
             local client = vim.lsp.get_client_by_id(event.data.client_id)
--- lua/mapping.lua
+++ lua/mapping.lua
@@ -3,3 +3,7 @@ local keymap = vim.keymap
 vim.g.mapleader = " "
 
 keymap.set("n", "<C-s>", ":w<cr>")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
+keymap.set("n", "J", ":m .+1<CR>==", { silent = true })
+keymap.set("n", "K", ":m .-2<CR>==", { silent = true })
