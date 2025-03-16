785c0e67f21b3a026b1a576f3df20bb9d10e08a2
Yurii Bilous <yurii.bilous@dxc.com>
2025-03-16 22:01:41 +0200

    mapping: add new key mapping
    Ctrl+A is used by tmux, therefore it conflicts
    with neovim mapping for number increment
    
    Remap Ctrl+A and Ctrl+X to Alt+A and Alt+X

--- lua/config/mapping.lua
+++ lua/config/mapping.lua
@@ -6,3 +6,6 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 keymap.set("n", "J", ":m .+1<CR>==", { silent = true })
 keymap.set("n", "K", ":m .-2<CR>==", { silent = true })
 keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+-- remap neovim increment/decrement to Alt+, as as Ctrl+A - is used by tmux
+keymap.set('n', '<A-a>', '<C-a>', { noremap = true, silent = true })
+keymap.set('n', '<A-x>', '<C-x>', { noremap = true, silent = true })
